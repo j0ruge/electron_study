5173669435e5d35410c8aae30307d690878cd4d8
JORGE LUIS DE FERRARI <joruge@gmail.com>
2024-05-15 18:45:01 -0300

    Refatoramento devido a problemas de imports

--- todos/index.js
+++ todos/index.js
@@ -1,10 +1,50 @@
 const electron = require('electron');
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 const path = require('path');
-const { app_menu, addWindow } = require('./main_menu')
+// const { app_menu } = require('./main_menu')
 
 let mainWindow;
 let webContent;
+let addWindow;
+
+const app_menu = [    
+    {
+        label: 'File',
+        submenu: [            
+            {
+                label: 'New Todo',
+                click() {createAddWindow()}
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click()
+                {
+                    app.quit();
+                }
+            }
+        ]
+    }
+]
+
+if (process.env.NODE_ENV !== 'production')
+    {
+        app_menu.push(
+            {
+            label: 'View', 
+            submenu: [
+                {
+                    label: 'Toggle Developer Tool',
+                    accelerator: process.platform == 'darwin' ? 'Command+Alt+I' : 'Ctrl+I',
+                    click(item, focusedWindow)
+                    {
+                        focusedWindow.toggleDevTools();
+                    }
+                }
+            ]
+            });  
+    }
+
 let main_menu = Menu.buildFromTemplate(app_menu);
 
 app.on('ready', () =>
@@ -27,10 +67,38 @@ app.on('ready', () =>
     mainWindow.on('close', () => app.quit());
 
     webContent = mainWindow.webContents;
-    webContent.openDevTools();
+    // webContent.openDevTools();
 });
 
+function createAddWindow()
+{
+    addWindow = new BrowserWindow(
+    {
+        width: 300, minWidth: 300, maxWidth: 300,
+        height: 200, minHeight: 200, maxHeight: 200,
+        title: 'Add New Todo',        
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+    });
 
+    // Carrega a interface do 'New Todo'    
+    // addWindow.loadFile(`${__dirname}/add/index.html`);
+    addWindow.loadURL(`file://${__dirname}${path.sep}add${path.sep}index.html`);
+    addWindow.on('closed', () => (addWindow = null));
+    //addWindow.webContents.openDevTools();
+
+    // Esconder a barra de Menu  nesta segunda janela. 
+    //addWindow.setMenuBarVisibility(false);
+}
+
+
+ipcMain.on('todo:add', (event, todo) => 
+{   console.log(todo);
+    webContent.send('todo:add', todo);    
+    addWindow.close();
+});
 
 
 // Main Menu
@@ -42,3 +110,8 @@ if(process.platform === 'darwin')
 
 
 Menu.setApplicationMenu(main_menu);
+
+module.exports =
+{
+    createAddWindow
+}
