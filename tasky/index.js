18d4dc04a4cbc49802c64b7578484117a888c4ad
JORGE LUIS DE FERRARI <joruge@gmail.com>
2024-05-20 19:51:43 -0300

    Classes

--- tasky/index.js
+++ tasky/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 let mainWindow;
 let tray;
+const HALF_WINDOWS_SIZE = 2;
 
 app.on('ready', () => 
 {
@@ -21,19 +22,29 @@ app.on('ready', () =>
     const iconPath = path.join(__dirname, `.${path.sep}src${path.sep}assets${path.sep}${iconName}`);
 
     tray = new Tray(iconPath);
-    tray.on('click', () =>
-    {
-        
-        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
-        // if (mainWindow.isVisible())
-        // {
+    tray.on('click', (event, bounds) =>
+    {        
+        // Click event bounds
+        const { x, y } = bounds;
 
-        // }
-        // else 
-        // {
-
-        // }
-        // mainWindow.show();
+        // Windows height and wi
+        const { width, height } = mainWindow.getBounds();
+         
+        if (mainWindow.isVisible())
+        {
+            mainWindow.hide();
+        }
+        else 
+        {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / HALF_WINDOWS_SIZE,
+                y: yPosition,
+                height: height,
+                width: width
+            });
+            mainWindow.show();
+        }        
     });
 });
 
