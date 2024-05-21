17516f0190edd6bf3fa4d71fa36e87979e6d6720
JORGE LUIS DE FERRARI <joruge@gmail.com>
2024-05-21 14:58:54 -0300

    Ajuste do Garbage Collector

--- tasky/index.js
+++ tasky/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const TimerTray = require('./app/timer_tray')
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => 
 {
@@ -20,7 +21,7 @@ app.on('ready', () =>
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `.${path.sep}src${path.sep}assets${path.sep}${iconName}`);
 
-    new TimerTray(iconPath, mainWindow);
+    tray = new TimerTray(iconPath, mainWindow); //Evita o Garbage Collector, limpar por não ter nehuma variável apontando para ela. 
 
 });
 
